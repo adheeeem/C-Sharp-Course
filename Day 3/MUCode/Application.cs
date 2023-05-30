c6c0c29621e6862f5b260a39677fe916a53ac9e3
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-05-30 19:55:49 +0500

    Changed repo structure

---
+++ Day 3/MUCode/Application.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace UnmanagedCode;
+
+public class Application:IDisposable
+{
+	private string filePath;
+	FileStream fs;
+	public Application(string filePath)
+	{
+		this.filePath = filePath;
+        fs = new FileStream(this.filePath, FileMode.OpenOrCreate);
+    }
+
+	public void Dispose()
+	{
+        fs.Close();
+    }
+
+	public void WriteToFile(string data)
+	{
+        byte[] strToByte = Encoding.UTF8.GetBytes(data + "\n");
+		fs.Write(strToByte, 0, strToByte.Length);
+
+    }
+
+
+	public string ReadFromFile()
+	{
+		byte[] b = new byte[1024];
+		StringBuilder res = new StringBuilder();
+		UTF8Encoding temp = new UTF8Encoding(true);
+		int readLen;
+		while ((readLen = fs.Read(b, 0, b.Length)) > 0)
+		{
+			res.Append(temp.GetString(b, 0, readLen));
+		}
+		return res.ToString();
+	}
+
+}
+
---
+++ Day 3/MUCode/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UnmanagedCode;
+
+while (true)
+{
+    Console.WriteLine("r - to read\nw - to write");
+    string option = Console.ReadLine();
+    if (option == "r")
+    {
+        using (Application app = new Application("input.txt"))
+        {
+            Console.WriteLine(app.ReadFromFile());
+        }
+    }
+    else if (option == "w")
+    {
+        using (Application app = new Application("input.txt"))
+        {
+            Console.Write("Input something: ");
+            string txt = Console.ReadLine();
+            app.WriteToFile(txt);
+        }
+
+    }
+}
