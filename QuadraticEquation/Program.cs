d1b4af553e5c8720edc6953552fd7ef49d6d1bce
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-05-30 12:16:28 +0500

    Add project files.

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Input b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Input c: ");
+double c = double.Parse(Console.ReadLine());
+
+double d = b * b - 4 * a * c;
+
+if (d < 0)
+{
+    Console.WriteLine("Discriminant is less than 0. Equation has no real roots");
+} else
+{
+    double x1, x2;
+    d = Math.Sqrt(d);
+    x1 = (-b + d) / 2;
+    x2 = (-b - d) / 2;
+    Console.WriteLine($"Discriminant={d}\nx1={x1}\nx2={x2}");
+}
---
+++ Warehouse/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] products = File.ReadAllLines("store.txt", Encoding.UTF8);
+        while (true)
+        {
+            Console.WriteLine("a - list of products\nb - add new product\nc - sell product\n");
+            string userInput = Console.ReadLine();
+            if (userInput == "a")
+            {
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product);
+                }
+            } else if (userInput == "b")
+            {
+                Console.Write("Input new product name: ");
+                string newProductName = Console.ReadLine();
+                Console.Write("Input new product quantity: ");
+                int newProductQuantity = int.Parse(Console.ReadLine());
+                bool flag = true;
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (products[i].StartsWith(newProductName)) {
+                        var temp = products[i].Split()[1];
+                        products[i] = newProductName + " " + (int.Parse(temp) + newProductQuantity);
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    string[] newProds = new string[products.Length + 1];
+                    for (int i = 0; i < products.Length; i++)
+                    {
+                        newProds[i] = products[i];
+                    }
+                    newProds[newProds.Length - 1] = newProductName + " " + newProductQuantity;
+                    products = newProds;
+                }
+                File.WriteAllLines("store.txt", products);
+            } else if (userInput == "c")
+            {
+                Console.Write("Input the product name you want to sell: ");
+                string productName = Console.ReadLine();
+                Console.Write("Input the quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+                bool productExists = false;
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (products[i].StartsWith(productName))
+                    {
+                        productExists = true;
+                        var temp = products[i].Split()[1];
+                        var check = int.Parse(temp) - productQuantity;
+                        if (check < 0)
+                        {
+                            Console.WriteLine("Not enough products in the list");
+                        }
+                        else
+                        {
+                            products[i] = productName + " " + check;
+                        }
+                    }
+                }
+                if (!productExists)
+                {
+                    Console.WriteLine("\nProduct does not exist");
+                }
+            }
+        }
+    }
+}
